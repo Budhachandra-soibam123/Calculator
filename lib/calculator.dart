139a991f11929cc9ac4ed6bbcd309c44928465eb
SanatombaM <Sanatomba143ba@gmail.com>
2023-02-14 16:04:04 +0530

    Calculator

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:math_expressions/math_expressions.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -8,7 +10,9 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  String number = "0";
+  String number = "";
+  String msg = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,272 +21,339 @@ class _CalculatorState extends State<Calculator> {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(number),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {
-                    number = "9";
-                    setState(() {});
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 50,
-                    child: Text(
-                      "9",
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(msg),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '9';
+                      setState(() {
+                        msg += number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      child: Text(
+                        "9",
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '8';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("8"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '7';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("7"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    onPressed: () {
+                      setState(() {
+                        msg = msg + "+";
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("+"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-              width: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+                width: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '6';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("6"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '5';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("5"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '4';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("4"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    onPressed: () {
+                      setState(() {
+                        msg = msg + "-";
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("-"),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-              width: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+                width: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '3';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("3"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '2';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("2"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '1';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("1"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  Container(
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.green),
-                      child: Text("*"),
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          msg = msg + "*";
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: Text("*"),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-              width: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
+                ],
+              ),
+              SizedBox(
+                height: 10,
+                width: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.grey),
-                      child: Text("C"),
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          msg = " ";
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: Text("C"),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black),
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      number = '0';
+                      setState(() {
+                        msg = msg + number;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
                       child: Text("0"),
-                      onPressed: () {},
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  Container(
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.green),
-                      child: Text("="),
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          Parser p = Parser();
+                          Expression exp = p.parse(msg);
+                          ContextModel ctx = ContextModel();
+                          double eva = exp.evaluate(EvaluationType.REAL, ctx);
+                          msg = eva.toString();
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: Text("="),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                  width: 10,
-                ),
-                InkWell(
-                  child: Container(
-                    color: Colors.black,
+                  SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                  Container(
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.green),
-                      child: Text("/"),
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          msg = msg + "/";
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: Text("/"),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+// Footer
+// © 2023 GitHub, Inc.
+// Footer navigation
+// Terms
+// Privacy
+// Security
+// Status
+// Docs
+// Contact GitHub
+// Pricing
+// API
+// Training
+// Blog
+// About
+// efg/calculator.dart at main · Budhachandra-soibam123/efg
---
+++ lib/funtion.dart
@@ -0,0 +1,13 @@
+import 'package:abc/calculator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CalFunction extends StatelessWidget {
+  const CalFunction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
